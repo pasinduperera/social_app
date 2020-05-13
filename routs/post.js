e3c25c879665f9566cbb4c567d5a19b2f31dc054
pasinduperera <pepasindu@gmail.com>
2020-05-14 01:53:41 +0530

    finished all post route..

--- routs/post.js
+++ routs/post.js
@@ -1,31 +1,3 @@
-// const express = require("express");
-// const router = express.Router(); 
-// const mongoose = require("mongoose");
-// //const Post = require('mongoose.model(Post)')
-// const require_login = require('../middleware/require_login')
-
-// router.post("./createpost",require_login,(req,res)=>{
-//     const{title,body} = req.body
-//     if(!title ||!body){
-//         return res.status(422).json({error :"have to enter title and body......."})
-//     }
-//     console.log(req.user);
-//     res.send("ok")
-
-//     // const post = new post({
-//     //     title,
-//     //     body,
-//     //     post      
-
-//     // })
-   
-// })
-// module.exports = router
-
-
-
-
-
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
@@ -33,136 +5,39 @@ const requireLogin = require('../middleware/require_login')
 const Post = mongoose.model("Post")
 
 
-// router.get('/allpost', requireLogin, (req, res) => {
-//     Post.find()
-//         .populate("postedBy", "_id name")
-//         .populate("comments.postedBy", "_id name")
-//         .sort('-createdAt')
-//         .then((posts) => {
-//             res.json({ posts })
-//         }).catch(err => {
-//             console.log(err)
-//         })
-
-// })
-
-// router.get('/getsubpost', requireLogin, (req, res) => {
-
-//     // if postedBy in following
-//     Post.find({ postedBy: { $in: req.user.following } })
-//         .populate("postedBy", "_id name")
-//         .populate("comments.postedBy", "_id name")
-//         .sort('-createdAt')
-//         .then(posts => {
-//             res.json({ posts })
-//         })
-//         .catch(err => {
-//             console.log(err)
-//         })
-// })
+router.get('/allpost',(req,res)=>{
+    Post.find()
+        .populate("postedBy","_id name")
+    .then(posts=>{
+        res.json({posts})
+    }).catch(err=>{console.log(err)})   
+})
 
 router.post('/createpost', requireLogin, (req, res) => {
     const { title, body} = req.body
     if (!title || !body) {
         return res.status(422).json({ error: "Plase add all the fields" })
     }
-    req.user.password = undefined
-    // const post = new Post({
-    //     title,
-    //     body,
-    //     photo: pic,
-    //     postedBy: req.user
-    // })
-    res.json('oky about that....')
-    res.send('okyyyy...')
-
-
-    // post.save().then(result => {
-    //     res.json({ post: result })
-    // })
-    //     .catch(err => {
-    //         console.log(err)
-    //     })
-
+   // req.user.password = undefined
+    //console.log(req.user)
+    //res.json({ message: " this is regurded to post request...", });
+    req.user.password =  undefined
+    const post = new Post({
+        title,
+        body,
+        postedBy : req.user
+    })
+    post.save().then(result=>{
+        res.json({post:result})
+    }).catch(err=>{
+        console.log(err)
+    })
 })
-
-// router.get('/mypost', requireLogin, (req, res) => {
-//     Post.find({ postedBy: req.user._id })
-//         .populate("PostedBy", "_id name")
-//         .then(mypost => {
-//             res.json({ mypost })
-//         })
-//         .catch(err => {
-//             console.log(err)
-//         })
-// })
-
-// router.put('/like', requireLogin, (req, res) => {
-//     Post.findByIdAndUpdate(req.body.postId, {
-//         $push: { likes: req.user._id }
-//     }, {
-//         new: true
-//     }).exec((err, result) => {
-//         if (err) {
-//             return res.status(422).json({ error: err })
-//         } else {
-//             res.json(result)
-//         }
-//     })
-// })
-
-// router.put('/unlike', requireLogin, (req, res) => {
-//     Post.findByIdAndUpdate(req.body.postId, {
-//         $pull: { likes: req.user._id }
-//     }, {
-//         new: true
-//     }).exec((err, result) => {
-//         if (err) {
-//             return res.status(422).json({ error: err })
-//         } else {
-//             res.json(result)
-//         }
-//     })
-// })
-
-
-// router.put('/comment', requireLogin, (req, res) => {
-//     const comment = {
-//         text: req.body.text,
-//         postedBy: req.user._id
-//     }
-//     Post.findByIdAndUpdate(req.body.postId, {
-//         $push: { comments: comment }
-//     }, {
-//         new: true
-//     })
-//         .populate("comments.postedBy", "_id name")
-//         .populate("postedBy", "_id name")
-//         .exec((err, result) => {
-//             if (err) {
-//                 return res.status(422).json({ error: err })
-//             } else {
-//                 res.json(result)
-//             }
-//         })
-// })
-
-// router.delete('/deletepost/:postId', requireLogin, (req, res) => {
-//     Post.findOne({ _id: req.params.postId })
-//         .populate("postedBy", "_id")
-//         .exec((err, post) => {
-//             if (err || !post) {
-//                 return res.status(422).json({ error: err })
-//             }
-//             if (post.postedBy._id.toString() === req.user._id.toString()) {
-//                 post.remove()
-//                     .then(result => {
-//                         res.json(result)
-//                     }).catch(err => {
-//                         console.log(err)
-//                     })
-//             }
-//         })
-// })
-
+router.get('/mypost', requireLogin,(req,res)=>{
+    Post.find({postedBy : req.user._id})
+    .populate("postedBy","_id name")
+    .then(mypost=>{
+        res.json({mypost})
+    })
+})
 module.exports = router
