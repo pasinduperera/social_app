192b2a603209ab87b3080dbd3a458aa66b45b26b
pasinduperera <pepasindu@gmail.com>
2020-05-18 01:49:18 +0530

    home ui

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -3,7 +3,72 @@ import React from 'react'
 const Home =()=>{
 
     return(
-        <h1>Home screen........</h1>
+      <div className="home">
+          <div className="card home-card">
+              <h5>Pasindu Perera</h5>
+              <div className="card image">
+                    <img style={{margin:"auto auto"}} src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+              </div>
+              <div className="card-content">
+                    <i className="material-icons " style={{ color: "red" }}>favorite</i>
+                  <h5>Title Here..</h5>
+                  <p> this is an amazing post...</p>
+                  <input type="text" placeholder="Add a comment..." />
+              </div>
+          </div>
+
+            <div className="card home-card">
+                <h5>Pasindu Perera</h5>
+                <div className="card image">
+                    <img style={{ margin: "auto auto" }} src="https://images.unsplash.com/photo-1558981285-6f0c94958bb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                </div>
+                <div className="card-content">
+                    <i className="material-icons " style={{ color: "red" }}>favorite</i>
+                    <h5>Title Here..</h5>
+                    <p> this is an amazing post...</p>
+                    <input type="text" placeholder="Add a comment..." />
+                </div>
+            </div>
+
+            <div className="card home-card">
+                <h5>Pasindu Perera</h5>
+                <div className="card image">
+                    <img style={{ margin: "auto auto" }} src="https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                </div>
+                <div className="card-content">
+                    <i className="material-icons " style={{ color: "red" }}>favorite</i>
+                    <h5>Title Here..</h5>
+                    <p> this is an amazing post...</p>
+                    <input type="text" placeholder="Add a comment..." />
+                </div>
+            </div>
+
+            <div className="card home-card">
+                <h5>Pasindu Perera</h5>
+                <div className="card image">
+                    <img style={{ margin: "auto auto" }} src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                </div>
+                <div className="card-content">
+                    <i className="material-icons " style={{ color: "red" }}>favorite</i>
+                    <h5>Title Here..</h5>
+                    <p> this is an amazing post...</p>
+                    <input type="text" placeholder="Add a comment..." />
+                </div>
+            </div>
+
+            <div className="card home-card">
+                <h5>Pasindu Perera</h5>
+                <div className="card image">
+                    <img style={{ margin: "auto auto" }} src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                </div>
+                <div className="card-content">
+                    <i className="material-icons " style={{ color:"red"}}>favorite</i>
+                    <h5>Title Here..</h5>
+                    <p> this is an amazing post...</p>
+                    <input type="text" placeholder="Add a comment..." />
+                </div>
+            </div>
+      </div>   
     );
 }
 
