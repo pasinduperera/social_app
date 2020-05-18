0ba65acb1b345d410b0e469197bb608dbdebc671
pasinduperera <pepasindu@gmail.com>
2020-05-19 02:35:45 +0530

    signup connection with backend

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link,useHistory } from "react-router-dom"
+import M from 'materialize-css'
 
 const Signup = () => {
+    const history = useHistory()
+    const [name,setName]=useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const PostData =()=>{
+        fetch("/signup",{method:"Post",
+        headers:{"Content-Type":"application/json"},
+    body:JSON.stringify({
+        name,
+        password,
+        email
+    })
+}).then(res=>res.json()).then(data=>{
+   if(data.error){  M.toast({ html:data.error,classes:"#d50000 red accent-4"})}
+   else{M.toast({ html: data.message, classes: "#00e5ff cyan accent-3" })
+        history.push('/signin')
+            }
 
+})
+    }
     return (
         <div className="mycard">
             <div className="card auth-card input-field #81c784 green lighten-2">
@@ -9,19 +30,30 @@ const Signup = () => {
                 <input
                     type="text"
                     placeholder="name"
+                    value={name}
+                    onChange={(e)=>setName(e.target.value)}
                 />
                 <input
                     type="text"
                     placeholder="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                 />
               
                 <input
                     type="text"
                     placeholder="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                 />
-                <button className="btn waves-effect waves-light #ff1744 red accent-3" type="submit" name="action">Signup
+                <button className="btn waves-effect waves-light #ff1744 red accent-3" type="submit" name="action"
+                onClick={()=>PostData()}
+                >Signup
 
                 </button>
+                <h5>
+                    <Link to='/signin'>Already have and account</Link>
+                </h5>
 
 
             </div>
